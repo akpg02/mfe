d24f7b4ac2669c6925d7b8de904e81067723ac3d
Grace Akpan <opps4lrng@gmail.com>
2025-06-02 17:33:22 -0600

    initial commit

---
+++ packages/container/src/app.js
@@ -0,0 +1,12 @@
+import React from "react";
+import MarketingApp from "./components/marketing-app";
+
+export default () => {
+  return (
+    <>
+      <div>Hi there....</div>
+      <hr />
+      <MarketingApp />
+    </>
+  );
+};
