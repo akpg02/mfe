75bdfadc2ac3d62fa6611ba442b9b04d79cd6b1c
Grace Akpan <opps4lrng@gmail.com>
2025-06-03 10:46:47 -0600

    add a unique classname to styling

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -2,15 +2,24 @@ import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import MarketingApp from "./components/marketing-app";
 import Header from "./components/Header";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
+
+// uniquely created classnames
+const generateClassName = createGenerateClassName({ productionPrefix: "co" });
 
 export default () => {
   return (
     <>
       <BrowserRouter>
-        <div>
-          <Header />
-          <MarketingApp />
-        </div>
+        <StylesProvider generateClassName={generateClassName}>
+          <div>
+            <Header />
+            <MarketingApp />
+          </div>
+        </StylesProvider>
       </BrowserRouter>
     </>
   );
