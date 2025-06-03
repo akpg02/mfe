e49927ab43f8f4297b64f552f59c0e8c8c0cb37b
Grace Akpan <opps4lrng@gmail.com>
2025-06-03 15:48:44 -0600

    Added routing between parent and child frontends

--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,26 +1,26 @@
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
-import MarketingApp from "./components/marketing-app";
-import Header from "./components/Header";
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import {
   StylesProvider,
   createGenerateClassName,
-} from "@material-ui/core/styles";
+} from '@material-ui/core/styles';
 
-// uniquely created classnames
-const generateClassName = createGenerateClassName({ productionPrefix: "co" });
+import MarketingApp from './components/MarketingApp';
+import Header from './components/Header';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
 
 export default () => {
   return (
-    <>
-      <BrowserRouter>
-        <StylesProvider generateClassName={generateClassName}>
-          <div>
-            <Header />
-            <MarketingApp />
-          </div>
-        </StylesProvider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
+    </BrowserRouter>
   );
 };
